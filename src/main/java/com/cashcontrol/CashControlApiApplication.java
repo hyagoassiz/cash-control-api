8d3336cba84717dece550abfd7d27ca0f7b1f777
hyagoassiz <hyagoassiz@hotmail.com>
2026-05-30 08:02:53 -0300

    first commit

---
+++ src/main/java/com/cashcontrol/CashControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.cashcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashControlApiApplication.class, args);
+	}
+
+}
